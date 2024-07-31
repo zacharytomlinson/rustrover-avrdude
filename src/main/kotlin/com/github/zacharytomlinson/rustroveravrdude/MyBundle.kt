f6580741238ec740b16f2df4ba6184a198018130
GitHub Action <action@github.com>
2024-07-31 16:54:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/zacharytomlinson/rustroveravrdude/MyBundle.kt
+++ src/main/kotlin/com/github/zacharytomlinson/rustroveravrdude/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zacharytomlinson.rustroveravrdude
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zacharytomlinson/rustroveravrdude/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/zacharytomlinson/rustroveravrdude/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zacharytomlinson.rustroveravrdude.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
